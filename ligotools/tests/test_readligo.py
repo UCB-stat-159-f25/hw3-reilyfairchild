cfb7f3c0510e48b21e796a08bce262c7ba2cde02
Reily Fairchild <reilyjean@berkeley.edu>
2025-11-06 06:45:34 +0000

    made my own ligo tests

--- ligotools/tests/test_readligo.py
+++ ligotools/tests/test_readligo.py
@@ -1,70 +1,43 @@
 from ligotools import readligo as rl
 import numpy as np
+import json
 
 
-def test_segment_creation():
-    """
-    Tests the dq_channel_to_seglist function.
-    This function turns a 1D array of 0s and 1s into a list of slices.
-    """
-    print("Running test_segment_creation...")
-    
-    # A simple channel with one segment of 1s
-    # Indices: 0  1  2  3  4  5
-    channel = np.array([0, 0, 1, 1, 1, 0])
-    
-    # We expect one segment (slice) from index 2 up to (but not including) 5
-    expected_slices = [slice(2, 5, None)]
-    
-    # Run the function with a sample frequency (fs) of 1
-    result_slices = rl.dq_channel_to_seglist(channel, fs=1)
-    
-    assert result_slices == expected_slices, f"Expected {expected_slices}, but got {result_slices}"
-    
-    # --- A second check with a different sample rate ---
-    
-    # We expect the same indices, but multiplied by the sample rate
-    expected_slices_fs4 = [slice(2 * 4, 5 * 4, None)] # slice(8, 20, None)
-    
-    result_slices_fs4 = rl.dq_channel_to_seglist(channel, fs=4)
-    
-    assert result_slices_fs4 == expected_slices_fs4, f"Expected {expected_slices_fs4}, but got {result_slices_fs4}"
+def test_loaddata():
+    eventname = 'GW150914' 
+    events = json.load(open("data/BBH_events_v3.json","r"))
+    event = events[eventname]
+    fn_H1 = 'data/'+event['fn_H1'] 
 
-    print("...test_segment_creation PASSED")
+    try:
+        # read in data from H1 and L1, if available:
+        strain_H1, time_H1, chan_dict_H1 = rl.loaddata(fn_H1, 'H1')
+        #strain_L1, time_L1, chan_dict_L1 = rl.loaddata(fn_L1, 'L1')
+    except:
+        print("Cannot find data files!")
+        print("You can download them from https://www.gwosc.org/s/events/"+eventname)
+        print("Quitting.")
+        quit()
+        
+    assert strain_H1.shape == (131072,)
+    print("...test_loaddata() PASSED")
 
 
-def test_gps_segment_conversion():
-    """
-    Tests the dq2segs function.
-    This function turns a 1D array into a SegmentList of (start_gps, stop_gps) tuples.
-    """
-    print("\nRunning test_gps_segment_conversion...")
-
-    # A channel with two segments
-    # Indices: 0  1  2  3  4  5  6  7
-    channel = np.array([0, 1, 1, 0, 1, 1, 1, 0])
-    
-    # The GPS start time of this channel
-    gps_start = 1000
-    
-    # Expected segments:
-    # 1. (1000 + index 1) to (1000 + index 3) -> (1001, 1003)
-    # 2. (1000 + index 4) to (1000 + index 7) -> (1004, 1007)
-    expected_seglist = [(1001, 1003), (1004, 1007)]
-    
-    # Run the function
-    result_segment_list_obj = rl.dq2segs(channel, gps_start)
-    
-    # Get the list of segments from the object
-    result_seglist = result_segment_list_obj.seglist
-    
-    assert result_seglist == expected_seglist, f"Expected {expected_seglist}, but got {result_seglist}"
-
-    print("...test_gps_segment_conversion PASSED")
-
+def test_dq():
+    eventname = 'GW150914' 
+    events = json.load(open("data/BBH_events_v3.json","r"))
+    event = events[eventname]
+    fn_L1 = 'data/'+event['fn_L1']  
+    strain, time, chan_dict = rl.loaddata(fn_L1, 'H1')
+    DQflag = 'CBC_CAT3'
+    # readligo.py method for computing segments (start and stop times with continuous valid data):
+    segment_list = rl.dq_channel_to_seglist(chan_dict[DQflag])
+    assert len(segment_list) == 1
+    print("...test_loaddata() PASSED")
 
 if __name__ == "__main__":
     print("--- Starting Simple Tests ---")
-    test_segment_creation()
-    test_gps_segment_conversion()
+    
+    test_loaddata()
+    test_dq()
     print("\n--- All Simple Tests Passed! ---")
